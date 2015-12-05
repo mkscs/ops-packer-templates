2a4b141c4f62c943991811841a3adf98a3c620d2
mkscs <mkscsy@gmail.com>
2015-12-05 21:22:01 +0100

    moving terraform to /opt/bin

--- scripts/VBOX/coreos/provision-terraform.sh
+++ scripts/VBOX/coreos/provision-terraform.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 curl -O https://releases.hashicorp.com/terraform/0.6.7/terraform_0.6.7_linux_amd64.zip
-sudo unzip terraform_0.6.7_linux_amd64.zip -d /opt
+sudo mkdir -p /opt/bin
+sudo unzip terraform_0.6.7_linux_amd64.zip -d /opt/bin
 rm -r terraform_0.6.7_linux_amd64.zip
